4cc918a75a479094e8dd657791c86778d145675b
NoahForTW <111335518+NoahForTW@users.noreply.github.com>
2024-11-19 18:00:59 +0800

    Level Design for Simple Core Functionality

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] private string switchScene;
+
+    public void StartButton()
+    {
+        SceneManager.LoadSceneAsync(switchScene);
+    }
+}

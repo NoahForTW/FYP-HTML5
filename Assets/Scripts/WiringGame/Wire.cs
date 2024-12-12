7d1817564e696842e648f45d6612cc77213b9cc2
Nitritious <123875011+Nitritious@users.noreply.github.com>
2024-12-12 11:20:10 +0800

    push wire script

---
+++ Assets/Scripts/WiringGame/Wire.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wire : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    bool Dragging = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Dragging)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 convertedMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            convertedMousePosition.z = 0;
+            transform.position = convertedMousePosition;
+
+            Vector3 positionDifference = convertedMousePosition - lineRenderer.transform.position;
+            lineRenderer.SetPosition(2, positionDifference);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Dragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        Dragging = false; 
+    }
+}

b7f4b2dfb1128f9f176a5f0e1e59fd8f06524e84
Nitritious <123875011+Nitritious@users.noreply.github.com>
2025-01-07 14:50:43 +0800

    Pushing script that controls the volume via game to volume mixers

---
+++ Assets/Scripts/VolumeSettings.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private Slider _musicSlider;
+    [SerializeField] private Slider _sfxSlider;
+
+    public void SetMusicVolume()
+    {
+        float musicVolume = _musicSlider.value;
+        _audioMixer.SetFloat("music", Mathf.Log10(musicVolume) *20);
+    }
+
+    public void SetSFXVolume()
+    {
+        float sfxVolume = _sfxSlider.value;
+        _audioMixer.SetFloat("music", Mathf.Log10(sfxVolume) * 20);
+    }
+}

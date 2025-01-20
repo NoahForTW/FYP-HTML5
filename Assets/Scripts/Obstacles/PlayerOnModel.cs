31e9a5a69397078bbf530495b5b214adcbbc539b
rachel <109144971+quiznakingray@users.noreply.github.com>
2025-01-20 19:44:17 +0800

    Edited player controls & levels

--- Assets/Scripts/Obstacles/PlayerOnModel.cs
+++ Assets/Scripts/Obstacles/PlayerOnModel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FerrisWheelCart : MonoBehaviour
+public class PlayerOnModel : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {

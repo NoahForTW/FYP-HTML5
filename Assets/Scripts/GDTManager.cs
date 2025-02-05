e377653c51ba7438965fcf5c6f17c8b906f100b4
NoahForTW <111335518+NoahForTW@users.noreply.github.com>
2025-02-05 11:45:35 +0800

    credit scene done

--- Assets/Scripts/GDTManager.cs
+++ Assets/Scripts/GDTManager.cs
@@ -8,12 +8,15 @@ public class GDTManager : MonoBehaviour
 {
     [SerializeField] private GameObject customisePanel;
     [SerializeField] private GameObject titlePanel;
+    [SerializeField] private GameObject creditsPanel;
 
+    [SerializeField] private Animator creditsAnimator;
 
     private void Start()
     {
         // Ensure the canvas starts hidden
         customisePanel.SetActive(false);
+        creditsPanel.SetActive(false);
         titlePanel.SetActive(true);
     }
 
@@ -24,10 +27,29 @@ public class GDTManager : MonoBehaviour
         titlePanel.SetActive(false);
     }
 
+    public void ToggleCredits()
+    {
+        creditsPanel.SetActive(true);
+        titlePanel.SetActive(false);
+
+        // Start the coroutine to wait for the animation to finish
+        StartCoroutine(WaitForCreditsAnimation());
+    }
+
     // To close Customise Screen
     public void CloseCustomise()
     {
         customisePanel.SetActive(false);
+        titlePanel.SetActive(true);
+    }
+
+    private IEnumerator WaitForCreditsAnimation()
+    {
+        // Wait for the animation to finish
+        yield return new WaitForSeconds(creditsAnimator.GetCurrentAnimatorStateInfo(0).length);
+
+        // After the animation is done, hide the credits panel and show the title panel
+        creditsPanel.SetActive(false);
         titlePanel.SetActive(true);
     }
 }
